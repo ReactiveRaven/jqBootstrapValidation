82feefacb1420c6bd38f32f883376a1655481cef
David Godfrey <davidmatthewgodfrey@gmail.com>
2012-05-18 20:10:30 +0100

    Including license terms

--- jqBootstrapValidation.js
+++ jqBootstrapValidation.js
@@ -4,6 +4,12 @@
  * http://www.github.com/ReactiveRaven/jqBootstrapValidation/
  */
 
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 (function( $ ){
 	
 	var defaults = {
