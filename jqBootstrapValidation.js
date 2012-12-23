d66d033fb865128b61c0fe5d0d8606af10c93e76
Reactive Raven <reactiveraven@gmail.com>
2012-12-23 07:48:26 +0000

    updating version number

--- jqBootstrapValidation.js
+++ jqBootstrapValidation.js
@@ -1,7 +1,7 @@
 /* jqBootstrapValidation
  * A plugin for automating validation on Twitter Bootstrap formatted forms.
  *
- * v1.3.4
+ * v1.3.6
  *
  * License: MIT <http://opensource.org/licenses/mit-license.php> - see LICENSE file
  *
