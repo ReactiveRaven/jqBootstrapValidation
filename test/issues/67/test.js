26eafe122bb2709787a416ef7a4d1af5f51d1fd7
David Godfrey <reactiveraven@gmail.com>
2013-05-03 14:15:21 +0100

    #67 test for performance issues

--- test/issues/67/test.js
+++ test/issues/67/test.js
@@ -54,35 +54,27 @@
         }
     });
 
-    module('required field', {
+    module('load test', {
         setup: function() {
-            $("#qunit-fixture").append($("\
-                <form class='form-horizontal' novalidate>\
-                    <div class='control-group'>\
-                        <label class='control-label'>Email address</label>\
-                        <div class='controls'>\
-                            <input\
-                                type='text'\
-                                name='input'\\n\
-                                data-validation-required-required='true'\
-                            />\
-                        </div>\
-                    </div>\
-                    <div class='form-actions'>\
-                        <button type='submit' class='btn btn-primary'>\
-                            Test Validation <i class='icon-ok icon-white'></i>\
-                        </button>\
-                    </div>\
-                </form>\
-            "));
+            $("#qunit-fixture").append($("#large-form").html());
             attachJqbv();
         },
         teardown: function() {
             $("#qunit-fixture").empty();
         }
     });
-    test('is required', 1 * numInJQBVTest, function() {
-        runJQBVTest("", [], ["error"], [], ["This is required"]);
+    test('runs quickly', 1, function() {
+        var $form = $("#qunit-fixture form").first();
+        var start = new Date();
+        $form.trigger("submit");
+        var elapsed = new Date() - start;
+        var inputCount = $("#qunit-fixture form").find("textarea,input,select").not("[type=submit]").length;
+        var timeAllowed = 20 * inputCount;
+        var percent = Math.floor((elapsed / timeAllowed) * 100) + "%";
+        ok(
+          elapsed < timeAllowed,
+          "Should run submit checks in less than " + timeAllowed + "ms - took " + elapsed + "ms; " + percent + " of budget."
+        );
     });
 
 }(jQuery));
