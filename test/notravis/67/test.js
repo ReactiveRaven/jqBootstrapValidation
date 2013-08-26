2c53878b1e9c9b64df6191b4f1a321d1905506c3
Chris Chua <chris@thousandeyes.com>
2013-08-26 12:40:58 -0700

    Fix global options in tests

--- test/notravis/67/test.js
+++ test/notravis/67/test.js
@@ -1,5 +1,5 @@
 /*global QUnit:false, module:false, test:false, asyncTest:false, expect:false, console:false*/
-/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global start:false, stop:false, ok:false, equal:false, notEqual:false, deepEqual:false*/
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 /*global JSON:false */
 /*global runJQBVTest:false, attachJqbv:false, numInJQBVTest:false, startJQBVTestQueue:false, pushJQBVTest:false, extractEvents:false*/
